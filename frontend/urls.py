efed7cf9d504d098ce463312ea7f27f21a91367e
Victor-Guerra <v.e.g.a.2000@hotmail.com>
2022-03-11 12:58:16 -0700

    Add base template for frontend pages.

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('login/', views.loginView , name='login'),
     path('signup/', views.signupView , name='signup'),
-    path('biorhythm/', views.biorhythmView , name='biorhythm'),
+    path('biorhythm/<int:user_id>', views.biorhythmView , name='biorhythm'),
 ] 
--- sytatyrbiorhythm/urls.py
+++ sytatyrbiorhythm/urls.py
@@ -18,6 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('api/', include('api.urls')),
-    path('', include('frontend.urls').),
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
