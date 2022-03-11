b844019b00c38f3535ed827cd96ef46dea650ba9
Victor-Guerra <v.e.g.a.2000@hotmail.com>
2022-03-11 12:58:16 -0700

    Add base template for frontend pages.

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.loginView , name='login'),
     path('signup/', views.signupView , name='signup'),
-    path('biorhythm/', views.biorhythmView , name='biorhythm'),
+    path('biorhythm/<int:user_id>', views.biorhythmView , name='biorhythm'),
 ] 
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.http import Http404
 
 
 def loginView(request):
@@ -15,8 +16,15 @@ def loginView(request):
 def signupView(request):
     pass
 
-def biorhythmView(request):
-    pass
+def biorhythmView(request, user_id =0):
+    if(user_id == 0):
+        raise Http404("Invalid user_id.")
+
+    template = loader.get_template("frontend/biorhythm.html"); 
+    context = {
+        'display_forecast': True
+    }
+    return HttpResponse(template.render(context, request))
 
 def schedulerView(request):
     pass
