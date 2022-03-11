176019bece47b881bc573084387eeb0ea267d823
GVSasa <Sebastiancitodelasierra@hotmail.com>
2022-03-10 22:18:57 -0700

    First Commite

--- frontend/views.py
+++ frontend/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+
 def loginView(request):
-    pass
+    display_forecast = False
+    template = loader.get_template('api/index.html')
+    context = {
+        'display_forecast': display_forecast,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def signupView(request):
     pass
