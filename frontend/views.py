0b39ffeb006c72a9840938102db022fa1e31fea3
GVSasa <Sebastiancitodelasierra@hotmail.com>
2022-03-12 10:09:08 -0700

    Frontend Finished

--- frontend/views.py
+++ frontend/views.py
@@ -6,7 +6,7 @@ from django.http import Http404
 
 def loginView(request):
     display_forecast = False
-    template = loader.get_template('api/index.html')
+    template = loader.get_template('frontend/login.html')
     context = {
         'display_forecast': display_forecast,
     }
@@ -14,13 +14,20 @@ def loginView(request):
 
 
 def signupView(request):
+    display_forecast = False
+    template = loader.get_template('frontend/base.html')
+    context = {
+        'display_forecast': display_forecast,
+    }
+    return HttpResponse(template.render(context, request))
+
     pass
 
 def biorhythmView(request, user_id =0):
     if(user_id == 0):
         raise Http404("Invalid user_id.")
 
-    template = loader.get_template("frontend/biorhythm.html"); 
+    template = loader.get_template("frontend/base.html"); 
     context = {
         'display_forecast': True
     }
