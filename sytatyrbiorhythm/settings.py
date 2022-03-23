5c1e3043be6c5da0c90be237ceedcd17ca2502ae
Victor-Guerra <v.e.g.a.2000@hotmail.com>
2022-03-23 16:48:02 -0700

    Updated requirements and settings for Heroku

--- sytatyrbiorhythm/settings.py
+++ sytatyrbiorhythm/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
